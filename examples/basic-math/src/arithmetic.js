3b2491b07ded796bde9a4987c8b17e432a852e65
Charlow <briankyalo416@gmail.com>
2025-03-24 12:50:01 +0300

    enhance arithmetic functions with type checks and error handling; add tests for edge cases

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,4 +1,18 @@
 export const add = (a, b) => {
+    if(typeof a === 'string') a = Number(a);
+    if(typeof b === 'string') b = Number(b);
+
+    if(typeof a == 'boolean' || typeof b == 'boolean') {
+        throw new Error("Input cannot be boolean");
+    }
+    if(Array.isArray(a) && Array.isArray(b)) {
+        return a.concat(b).join(",");
+    }
+
+    
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Cannot convert input to number");
+    }
     return a + b;
 };
 
@@ -11,5 +25,9 @@ export const multiply = (a, b) => {
 };
 
 export const divide = (a, b) => {
+
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Cannot convert input to number");
+    }
     return a / b;
 };
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -5,22 +5,59 @@ describe('add', () => {
     it("should add two numbers", () => {
         expect(add(1, 2)).toBe(3);
     });
+    it("should add two negative numbers", () => {
+        expect(add(-1, -2)).toBe(-3);
+    });
+    it("should parse strings into integers", () => {
+        expect(add("1", "2")).toBe(3);
+    });
+    it("should get angry when given a string that cannot be changed to a number", () => {
+        expect(() => add("2", "two")).toThrow();
+    })
+    it("should throw an error when a boolean value is passed", () => {
+        expect(() => add(2, true)).toThrow("Input cannot be boolean");
+    })
+    it("should accept an input of arrays and concatenate them", () => {
+        expect(add([1, 2], [3, 4])).toBe("1,2,3,4");
+    })
 });
 
 describe('subtract', () => {
     it("should subtract two numbers", () => {
         expect(subtract(2, 1)).toBe(1);
     });
+    it("should subtract two negative numbers", () => {
+        expect(subtract(-1, -2)).toBe(1);
+    });
+    
 });
 
 describe('multiply', () => {
     it("should multiply two numbers", ()=>{
         expect(multiply(2, 3)).toBe(6);
     })
+    it("should multiply two negative numbers", ()=>{
+        expect(multiply(-2, -3)).toBe(6);
+    })
+    it("should convert strings to numbers", ()=>{
+        expect(multiply("2", "3")).toBe(6);
+    })
 });
 
 describe('divide', () => {
     it("should divide two numbers", ()=>{
         expect(divide(6, 3)).toBe(2);
+    })
+    it("should divide given numbers by 0 to give undefined", () => {
+        expect(divide(8, 0)).toBe(Infinity);
+    })
+    it("should divide two negative numbers", ()=>{
+        expect(divide(-6, -3)).toBe(2);
+    }),
+    it("should convert strings to numbers", ()=>{
+        expect(divide("6", "3")).toBe(2);
+    })
+    it("should throw an error if input cannot be converted to a number", () => {
+        expect(() => divide("6", "three")).toThrow("Cannot convert input to number");
     })
 });
