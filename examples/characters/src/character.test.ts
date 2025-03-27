33d8b915bab5669594b9ef1584acdbc4d3d43e94
Charlow <briankyalo416@gmail.com>
2025-03-27 09:20:24 +0300

    refactor character tests for improved readability and consistency; add additional property checks

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -2,29 +2,52 @@ import { describe, it, expect } from 'vitest';
 import { Character } from './character.js';
 import { Person } from './person.js';
 
+
+
+const firstName = 'John';
+const lastName = 'Doe';
+const role = 'Warrior';
+
 describe('Character', () => {
+
+  let character;
+
+  beforeEach(() => {
+     character = new Character(firstName, lastName, role);
+  });
   it(
     'should create a character with a first name, last name, and role',
     () => {
-      const character = new Character('John', 'Doe', 'Warrior');
+
+     
       expect(character).toBeInstanceOf(Character);
       expect(character).toBeInstanceOf(Person);
-      expect(character.firstName).toBe('John');
-      expect(character.lastName).toBe('Doe');
-      expect(character.role).toBe('Warrior');
+      expect(character.firstName).toBe(firstName);
+      expect(character.lastName).toBe(lastName);
+      expect(character.role).toBe(role);
+
+      expect(character).toEqual(expect.objectContaining({
+        firstName,
+        lastName,
+        role,
+        charisma: expect.any(Number),
+        strength: expect.any(Number),
+        dexterity: expect.any(Number),
+        createdAt: expect.any(Date),
+      }));
       
     },
   );
 
   it('should allow you to increase the level', () => {
-    const character = new Character('Brian', 'Kyalo', 'Warrior');
+   
     character.levelUp();
     expect(character.level).toBe(2);
   });
 
   it('should update the last modified date when leveling up', (
   ) => {
-    const character = new Character('Brian', 'Kyalo', 'Warrior');
+  
     const prevModified = character.lastModified;
     character.levelUp();
     expect(character.lastModified).not.toBe(prevModified);
