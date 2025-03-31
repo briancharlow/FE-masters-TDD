ccc12670cde1f40671c89112442afcfe96a24d35
Charlow <briankyalo416@gmail.com>
2025-03-31 17:31:13 +0300

    refactor button tests to use testing-library for improved clarity and reliability

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,20 +1,35 @@
 import { createButton } from './button.js';
+import { fireEvent, screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 
 describe('createButton', () => {
+
+  afterEach(() => {
+    document.body.innerHTML = ''; // Clear the DOM after each test
+  })
   it('should create a button element', () => {
-    const button = createButton();
-    expect(button).toBeInstanceOf(HTMLButtonElement);
-    expect(button.tagName).toBe('BUTTON');
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
+ 
   });
 
   it('should have the text "Click Me"', () => {
-    const button = createButton();
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
     expect(button.textContent).toBe('Click Me');
   });
-
   it('should change the text to "Clicked!" when clicked', async () => {
-    const button = createButton();
-    button.click();
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
+    await userEvent.click(button);
+    // fireEvent(button, new MouseEvent('click')
+    // );
     expect(button.textContent).toBe('Clicked!');
   });
 });
